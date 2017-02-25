b02f61a2a7ba452d1d9cb86cacb2844625aa4323
Lindsay Gilbert <Lindsaygilbert722@gmail.com>
2017-02-24 16:31:12 -0800

    added data to chart

--- app.js
+++ app.js
@@ -34,17 +34,6 @@ for (var i = 0; i < imageArrayName.length; i++) {
 
 // //Set up function to come up with a random number corresponding to imageArrayPath index
 function getRandImage() {
-//   var randomImageIndices = [];
-//   while(randomImageIndices.length < 3) {
-//     Math.floor(Math.random() * (totProducts.length + 1));
-//     var possIndex = Math.floor(Math.random() * (totProducts.length + 1));
-//     console.log('possIndex', possIndex);
-//     if (randomImageIndices.indexOf(possIndex) === -1) {
-//       randomImageIndices.push(possIndex);
-//     }
-//   }
-//   return randomImageIndices;
-//   console.log('randomImageIndices', randomImageIndices);
   return Math.floor(Math.random() * (totProducts.length));
 }
 // getRandImages();
@@ -118,7 +107,7 @@ function picClicks() {
 function displayList() {
   for (var j = 0; j < totProducts.length; j++) {
     var liEl = document.createElement('li');
-    liEl.textContent = totProducts[j].name + 'has been clicked' + totProducts[j].clicks;
+    liEl.textContent = totProducts[j].name + ' has been clicked ' + totProducts[j].clicks;
     resultsEl.appendChild(liEl);
     localStorage.totalProducts = JSON.stringify(totProducts);
   }
@@ -126,138 +115,3 @@ function displayList() {
   cent.removeEventListener('click', picClicks);
   right.removeEventListener('click', picClicks);
 };
-//
-// //Set up function to procure a random number corresponding to imageArrayPath index
-// function randImage() {
-//   return Math.floor(Math.random() * totProducts.length);
-// }
-//
-// //Function to display new images
-// function displayPic() {
-//   var leftIm = randImage();
-//   var centIm = randImage();
-//   var rightIm = randImage();
-//
-//   while (centIm === leftIm) {
-//     centIm = randImage();
-//   }
-//   while (rightIm === centIm || rightIm === leftIm) {
-//     rightIm = randImage();
-//   }
-//
-//   var leftProd = totProducts[leftIm];
-//   leftProd.views += 1;
-//
-//   var centProd = totProducts[centIm];
-//   centProd.views += 1;
-//
-//   var rightProd = totProducts[rightIm];
-//   rightProd.views += 1;
-//
-//   console.log('leftProd', leftProd);
-// }
-//
-// displaypic();
-// //For loop creating a new array with random images from above
-// for (var i = 0; i < randomImageIndices.length; i++){
-//   var imageString = randomImageIndices[i];
-//   console.log('imageString', imageString);
-//   var leftImage = randomImageIndices[0];
-//   var centImage = randomImageIndices[1];
-//   var rightImage = randomImageIndices[2];
-// }
-//
-// //Function to show images
-// function showLeftImage() {
-//   document.write('<img src=" ' + leftImage + ' ">');
-// }
-//
-// function showCentImage() {
-//   document.write('<img src=" ' + centImage + ' ">');
-// }
-//
-// function showRightImage() {
-//   document.write('<img src=" ' + rightImage + ' ">');
-// }
-//
-// var k = 0;
-// var imageGen = [];
-// for (var i = 0; i < imageArrayPath.length; i++) {
-//   imageGen[i] = new Image();
-//   imageGen[i].src = imageArrayPath[i];
-// }
-//
-// for (var i = 0; i < imageArrayPath.length; i++) {
-//   var newImage = new ChoiceImage(imageArrayPath[i].name, imageArrayPath[i].path);
-//   console.log('newImage', newImage);
-// }
-//
-// imageArrayPath[0] = 'img/assets/bag.jpg';
-// imageArrayPath[1] = 'img/assets/banana.jpg';
-//
-// function ChoiceImage(path) {
-//   this.path = path;
-//   this.clicks = 0;
-//   this.display = 0;
-// }
-//
-// for (var i = 0; i < imageArrayPath.length; i++){
-//   var imageString = imageArrayPath[i];
-//   console.log(imageString);
-// }
-//
-// var k = 0;
-// var imageGen = new Array();
-// for (var i = 0; i < imageArrayPath.length; i++) {
-//   imageGen[i] = new Image();
-//   imageGen[i].src = imageArrayPath[i];
-// }
-// var someImage = Math.round(Math.random() * (imageArrayPath.length - 1));
-// function showImage() {
-//   document.write('<img src=" ' + imageArrayPath[someImage] + ' ">');
-// }
-// var someImage2 = Math.round(Math.random() * (imageArrayPath.length - 1));
-// if (someImage2 === someImage) {
-//   var someImage2 = Math.round(Math.random() * (imageArrayPath.length - 1));
-// } else {
-//   function showImage2() {
-//     document.write('<img src=" ' + imageArrayPath[someImage2] + ' ">');
-//   }
-// }
-// var someImage3 = Math.round(Math.random() * (imageArrayPath.length - 1));
-// function showImage3() {
-//   if (someImage3 === someImage || someImage3 === someImage2) {
-//     var someImage3 = Math.round(Math.random() * (imageArrayPath.length - 1));
-//   } else {
-//     document.write('<img src=" ' + imageArrayPath[someImage3] + ' ">');
-//   }
-// }
-//
-//Set up function to come up with a random number corresponding to imageArrayPath index
-// function getRandImages() {
-//   var randomImageIndices = [];
-//   while(randomImageIndices.length < 3) {
-//     Math.floor(Math.random() * (totProducts.length + 1));
-//     var possIndex = Math.floor(Math.random() * (totProducts.length + 1));
-//     console.log('possIndex', possIndex);
-//     if (randomImageIndices.indexOf(possIndex) === -1) {
-//       randomImageIndices.push(possIndex);
-//     }
-//   }
-//   return randomImageIndices;
-// }
-//
-// console.log('someImage3', someImage3);
-// console.log('someImage', someImage);
-// console.log('someImage2', someImage2);
-// for (var j = 0; j < 3; j++) {
-//   showImage(j);
-// }
-
-//function displayList() {
-//  var picList = document.getElementById('picList')
-// }
-//
-// function saveProductsToLocalStorage(allProducts) {
-//   localStorage.
-// }
